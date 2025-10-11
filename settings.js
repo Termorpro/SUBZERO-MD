1dbf86d62eb0a09c9088f26218e9af7b948723c9
Termorpro <164052650+Termorpro@users.noreply.github.com>
2025-10-11 14:31:21 +0300

    Update settings.js for SuperMn

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "SUBZERO-MD;;;uE8QFDAS#MGa0cXlKFd2MrPtj1wpyyGtbuuCc2XRWnEVJXrBktzc",
+  OWNER_NUMBER: "+256784608824", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
